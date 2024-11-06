ac8dcb4d575cbb3e68574298b153ed8a99192272
mgucma <marek.gucma@gmail.com>
2024-11-06 09:46:30 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let numberOfCalls = 0;
+
+  return (numberOne = 0) => {
+    return (numberTwo = 0) => {
+      numberOfCalls++;
+
+      const numbersSum = numberOne + numberTwo;
+
+      return numberOfCalls > 3 && numberOfCalls % 2 === 0
+        ? `Bzzz... Error!`
+        : numbersSum;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
